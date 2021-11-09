0eb55837a922c58b890ded6baf75c3e51e281ca5
Tywa90 <amserega90@gmail.com>
2021-11-09 21:42:25 +0200

    vid-043 modal Windows

--- JS-03/043 Food new windows/js/script.js
+++ JS-03/043 Food new windows/js/script.js
@@ -40,7 +40,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     //Timer
 
-    const deadline = '2021-11-10';
+    const deadline = '2022-01-01';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -96,8 +96,38 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
     setClock('.timer', deadline);
-    const ttt = new Date();
-    console.log(ttt.getUTCHours());
-    console.log(ttt.getHours());
+    
 
+    //Modal
+    const modalTriger = document.querySelectorAll('[data-modal]'),
+         modal = document.querySelector('.modal'),
+         modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTriger.forEach(btn => {
+        btn.addEventListener('click', () => {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModal () {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+     
 });
